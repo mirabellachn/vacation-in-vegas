44ed9d844a39382b320ac619e08e3ef1e4b62150
mirabellachn <mirabellachn@gmail.com>
2024-10-21 21:40:03 +0700

    Initial Commit

---
+++ VacationInVegas/Scrolling.swift
@@ -0,0 +1,33 @@
+//
+//  ScrollImage.swift
+//  VacationInVegas
+//
+//  Created by mirabella  on 19/10/24.
+//
+
+import SwiftUI
+
+struct Scrolling: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                ScrollImage(image: "bellagio")
+                
+                ScrollImage(image: "excalibur")
+                
+                ScrollImage(image: "luxor")
+                
+                ScrollImage(image: "paris")
+                
+                ScrollImage(image: "stratosphere")
+                
+                ScrollImage(image: "treasureisland")
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    Scrolling()
+}
