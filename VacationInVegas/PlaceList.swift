10fa1c9716db0d55863ae42f6f5c9d4e1c187409
mirabellachn <mirabellachn@gmail.com>
2024-10-23 23:26:41 +0700

    create list of images with interested

---
+++ VacationInVegas/PlaceList.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  VacationInVegas
+//
+//  Created by mirabella  on 18/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct PlaceList: View {
+    @Query(sort: \Place.name) private var places:
+    [Place]
+    
+    @State private var showImages = false
+    
+    var body: some View {
+        NavigationStack {
+            List(places) { place in
+                HStack {
+                    place.image
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(.rect(cornerRadius: 7))
+                        .frame(width:100, height:100)
+                    
+                    Text(place.name + "test commit")
+                    
+                    Spacer()
+                    
+                    if place.interested {
+                        Image(systemName: "star.fill")
+                            .foregroundStyle(.yellow)
+                            .padding(.trailing)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PlaceList()
+        .modelContainer(Place.preview)
+}
